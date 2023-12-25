7bd402252176c392ca0c3cde89561e4eb20b8aa0
goonmandu <hwang.mingi7@gmail.com>
2023-12-25 16:04:24 -0500

    Day 3 start

---
+++ src/03/main.py
@@ -0,0 +1,20 @@
+from typing import TextIO, Any
+
+
+def parse(file: TextIO) -> list[Any]:
+    pass
+
+
+def part1(data: list[Any]) -> None:
+    pass
+
+
+def part2(data: list[Any]) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    with open("./input.txt", "r") as f:
+        puzzle = parse(f)
+        part1(puzzle)
+        part2(puzzle)
